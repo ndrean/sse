f672d9f7880d730af2108e8387c5dc02aeee2c7a
neven <nevendrean@yahoo.fr>
2022-07-26 00:08:51 +0200

    version heroku

--- lib/application.ex
+++ lib/application.ex
@@ -2,52 +2,52 @@ defmodule SSE.Application do
   use Application
 
   def start(_type, _args) do
-    plug_options =
-      case app_port() do
-        4000 ->
-          [
-            port: app_port(),
-            compress: false,
-            protocol_options: [idle_timeout: :infinity]
-          ]
+    # plug_options =
+    #   case app_port() do
+    #     4000 ->
+    #       [
+    #         port: app_port(),
+    #         compress: false,
+    #         protocol_options: [idle_timeout: :infinity]
+    #       ]
 
-        4001 ->
-          [
-            port: 4001,
-            cipher_suite: :strong,
-            certfile: "priv/cert/selfsigned.pem",
-            keyfile: "priv/cert/selfsigned_key.pem",
-            otp_app: :sse,
-            password: "SECRET"
-          ]
+    #     4001 ->
+    #       [
+    #         port: 4001,
+    #         cipher_suite: :strong,
+    #         certfile: "priv/cert/selfsigned.pem",
+    #         keyfile: "priv/cert/selfsigned_key.pem",
+    #         otp_app: :sse,
+    #         password: "SECRET"
+    #       ]
 
-        443 ->
-          [port: 443, protocol_options: [idle_timeout: :infinity]]
-      end
+    #     443 ->
+    #       [port: 443, protocol_options: [idle_timeout: :infinity]]
+    #   end
 
-    scheme =
-      case app_port() do
-        4000 -> :http
-        4001 -> :https
-        443 -> :heroku
-      end
+    # scheme =
+    #   case app_port() do
+    #     4000 -> :http
+    #     4001 -> :https
+    #     443 -> :heroku
+    #   end
 
     children = [
-      {Plug.Cowboy, scheme: scheme, plug: SSE.Router, options: plug_options},
-      # {Plug.Cowboy,
-      #  scheme: :https,
-      #  plug: SSE.Router,
-      #  options: [protocol_options: [idle_timeout: :infinity], port: 443]},
+      # {Plug.Cowboy, scheme: scheme, plug: SSE.Router, options: plug_options},
+      {Plug.Cowboy,
+       scheme: :https,
+       plug: SSE.Router,
+       options: [protocol_options: [idle_timeout: :infinity], port: 443]},
       PubSub
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: SSE.Supervisor)
   end
 
-  defp app_port do
-    System.get_env()
-    |> Map.get("PORT", "4000")
-    # default value
-    |> String.to_integer()
-  end
+  # defp app_port do
+  #   System.get_env()
+  #   |> Map.get("PORT", "4000")
+  #   # default value
+  #   |> String.to_integer()
+  # end
 end
--- lib/plug_sse_headers.ex
+++ lib/plug_sse_headers.ex
@@ -3,38 +3,39 @@ defmodule Plug.SSEHeaders do
 
   def init(opt), do: opt
 
-  def call(conn, opt) do
-    case get_scheme() do
-      :http ->
-        IO.inspect(opt[:front])
+  def call(conn, _opt) do
+    # case get_scheme() do
+    #   :http ->
+    #     IO.inspect(opt[:front])
 
-        conn
-        |> put_resp_header("Access-Control-Allow-Origin", opt[:front])
-        |> put_resp_header("Vary", "Origin")
-        |> put_resp_header("Cache-Control", "no-cache")
-        |> put_resp_header("connection", "keep-alive")
-        |> put_resp_header("content-type", "text/event-stream")
+    #     conn
+    #     |> put_resp_header("Access-Control-Allow-Origin", opt[:front])
+    #     |> put_resp_header("Vary", "Origin")
+    #     |> put_resp_header("Cache-Control", "no-cache")
+    #     |> put_resp_header("connection", "keep-alive")
+    #     |> put_resp_header("content-type", "text/event-stream")
 
-      :https ->
-        IO.puts("https")
+    #   :https ->
+    #     IO.puts("https")
 
-        conn
-        |> put_resp_header("connection", "keep-alive")
-        |> put_resp_header("content-type", "text/event-stream")
-    end
+    conn
+    |> put_resp_header("connection", "keep-alive")
+    |> put_resp_header("content-type", "text/event-stream")
+
+    # end
   end
 
-  defp get_scheme() do
-    port =
-      System.get_env()
-      |> Map.get("PORT", "4000")
-      |> String.to_integer()
-      |> IO.inspect()
+  # defp get_scheme() do
+  #   port =
+  #     System.get_env()
+  #     |> Map.get("PORT", "4000")
+  #     |> String.to_integer()
+  #     |> IO.inspect()
 
-    case port do
-      4000 -> :http
-      4001 -> :https
-      443 -> :https
-    end
-  end
+  #   case port do
+  #     4000 -> :http
+  #     4001 -> :https
+  #     443 -> :https
+  #   end
+  # end
 end
